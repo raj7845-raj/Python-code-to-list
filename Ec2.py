1baf9a5ff5ab35f1c59032432659fb8c2cc8ff4d
raj7845-raj <61011683+raj7845-raj@users.noreply.github.com>
2020-03-29 17:49:41 -0500

    Add files via upload

---
+++ Ec2.py
@@ -0,0 +1,17 @@
+import boto3
+
+access_key = "AKIA25ATJSTNHOBZLTGH"
+secret_key = "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"
+
+client = boto3.client('ec2', aws_access_key_id=access_key, aws_secret_access_key=secret_key,
+region_name='us-east-1')
+
+ec2_regions = [region['RegionName'] for region in client.describe_regions()['Regions']]
+
+for region in ec2_regions:
+conn = boto3.resource('ec2', aws_access_key_id=access_key, aws_secret_access_key=secret_key,
+region_name=region)
+instances = conn.instances.filter()
+for instance in instances:
+if instance.state["Name"] == "running":
+print (instance.id, instance.instance_type, region)
